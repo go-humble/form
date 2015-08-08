f0da030e1e9cc4fa9d76a5d319e3ec255d540753
Alex Browne <stephenalexbrowne@gmail.com>
2015-08-08 15:52:00 -0700

    Implement Time and GetTime. Make Parse accept dom.Element to make it easier on callers.

--- input.go
+++ input.go
@@ -2,6 +2,7 @@ package form
 
 import (
 	"strconv"
+	"time"
 
 	"honnef.co/go/js/dom"
 )
@@ -14,16 +15,21 @@ type Input struct {
 }
 
 func NewInput(el *dom.HTMLInputElement) *Input {
+	// Attempt to determine the type by first getting the type attribute
+	// directly. This is more reliable as some browsers will always return
+	// "text" as the type if the type attribute is not recognized/supported.
+	// If the type attribute is missing, fallback to using what the browser
+	// thinks the type is (which is probably "text").
+	inputType := InputType(el.GetAttribute("type"))
+	if inputType == "" {
+		inputType = InputType(el.Type)
+	}
 	return &Input{
 		El:       el,
 		Name:     el.Name,
 		RawValue: el.Value,
-		Type:     InputType(el.Type),
+		Type:     inputType,
 	}
-}
-
-func (input Input) String() string {
-	return input.RawValue
 }
 
 func (input Input) Int() (int, error) {
@@ -40,5 +46,21 @@ func (input Input) Bool() (bool, error) {
 		return input.El.Checked, nil
 	default:
 		return strconv.ParseBool(input.RawValue)
+	}
+}
+
+const (
+	rfc3339DateLayout          = "2006-01-02"
+	rfc3339DatetimeLocalLayout = "2006-01-02T15:04:05.999999999"
+)
+
+func (input Input) Time() (time.Time, error) {
+	switch input.Type {
+	case InputDate:
+		return time.Parse(rfc3339DateLayout, input.RawValue)
+	case InputDateTimeLocal:
+		return time.Parse(rfc3339DatetimeLocalLayout, input.RawValue)
+	default:
+		return time.Parse(time.RFC3339, input.RawValue)
 	}
 }
