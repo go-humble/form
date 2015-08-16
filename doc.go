105d74e79fdd5782780e1b9419d5485b8d4e139d
Alex Browne <stephenalexbrowne@gmail.com>
2015-08-15 21:16:40 -0700

    Bump version to 0.0.1

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@
 // great as a stand-alone package or in combination with other Humble packages
 // (https://github.com/go-humble).
 //
-// Version X.X.X (develop)
+// Version 0.0.1
 //
 // For the full source code, a quickstart guide, and more information visit
 // https://github.com/go-humble/form.
